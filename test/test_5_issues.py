f3a63c949118e9261b761a5145073d12593be006
Jonathan Reichelt Gjertsen <jonathan.gjertsen@disruptive-technologies.com>
2020-12-31 18:12:42 +0100

    Make test more sensible

--- test/test_5_issues.py
+++ test/test_5_issues.py
@@ -4,7 +4,7 @@ from jchord.chords import ChordWithRoot, InvalidChord
 from jchord.progressions import ChordProgression
 
 
-def make_midi_file(chord_names, out_file):
+def make_midi_file(chord_names, beats_per_chord, out_file):
     chords = []
     for name in chord_names:
         try:
@@ -14,9 +14,7 @@ def make_midi_file(chord_names, out_file):
         chords.append(chord)
 
     progression = ChordProgression(chords)
-    progression.to_midi(
-        out_file, instrument=11, beats_per_chord=[1, 2, 1, 2, 0.5, 0.25, 0.75, 2]
-    )
+    progression.to_midi(out_file, instrument=11, beats_per_chord=beats_per_chord)
 
 
 def test_github_issue_56():
@@ -30,11 +28,12 @@ def test_github_issue_56():
 
     # Data from the image
     names = ["N", "B:min", "B:min", "B:min", "B:min", "G:maj", "G:maj", "G:maj"]
+    beats_per_chord = [1, 2, 1, 2, 0.5, 0.25, 0.75, 2]
     # Convert names to what the library expects
     names = [name.replace(":", "") for name in names]
 
     # Generate a file
-    make_midi_file(names, midi_filename_generated)
+    make_midi_file(names, beats_per_chord, midi_filename_generated)
 
     with open(midi_filename_generated, "rb") as generated_file:
         generated_bytes = generated_file.read()
