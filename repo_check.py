c556355b5a017a648a902b43ff0565084651f639
Jonathan Reichelt Gjertsen <jonathan.gjertsen@disruptive-technologies.com>
2021-08-08 15:10:29 +0200

    Restructure repo_check bit to make things simpler

--- repo_check.py
+++ repo_check.py
@@ -2,51 +2,75 @@ from pathlib import Path
 from subprocess import run
 import sys
 
+TASKS = {}
+def task(func):
+    TASKS[func.__name__] = func
+    return func
 
-def fix_crlf(path):
-    path_str = str(path)
-    if any(
-        token in path_str
-        for token in (".git", ".pytest_cache", "build", "dist", ".png", ".mid")
-    ):
-        return
-    if path.is_dir():
-        for subpath in path.iterdir():
-            fix_crlf(subpath)
-    else:
-        try:
-            path.write_text(path.read_text().replace("\r\n", "\n"))
-        except UnicodeDecodeError:
-            pass
+@task
+def build_docs():
+    print("========== Generating docs...")
+    proc = run(["sphinx-build", ".", "docs", "-c", "docs"])
+    if proc.returncode != 0:
+        print("sphinx-build failed.")
+        sys.exit(1)
 
-
-if __name__ == "__main__":
+@task
+def fix_crlf():
     print("========== Removing CRLF...")
-    fix_crlf(Path("."))
+    def helper(path):
+        path_str = str(path)
+        if any(
+            token in path_str
+            for token in (".git", ".pytest_cache", "build", "dist", ".png", ".mid")
+        ):
+            return
+        if path.is_dir():
+            for subpath in path.iterdir():
+                fix_crlf(subpath)
+        else:
+            try:
+                path.write_text(path.read_text().replace("\r\n", "\n"))
+            except UnicodeDecodeError:
+                pass
+    helper(Path("."))
 
-    print("========== Checking if git repo is clean...")
-    porcelain_initial = (
+@task
+def check_clean():
+    print("========== Checking if anything changed...")
+    porcelain_after = (
         run(["git", "status", "--porcelain"], capture_output=True)
         .stdout.decode("ascii", errors="ignore")
         .strip()
     )
-    if porcelain_initial:
-        print(porcelain_initial)
-        print("Git repo is dirty.")
+    if porcelain_after:
+        print(porcelain_after)
+        run(["git", "status", "-vvv"])
+        print(
+            "Repo is dirty.\n"
+            "If this is run locally, please commit the files that were updated.\n"
+            "If this is in CI, please run python repo_check.py locally and commit the changes."
+        )
         sys.exit(1)
 
+@task
+def format():
     print("========== Formatting python code...")
     proc = run(["black", "."])
     if proc.returncode != 0:
         print("black failed.")
         sys.exit(1)
 
+@task
+def pytest():
     print("========== Checking if tests pass...")
     proc = run(["pytest"])
     if proc.returncode != 0:
         print("pytest failed.")
         sys.exit(1)
 
+@task
+def doctest():
     print("========== Checking if doctests pass...")
     proc = run(
         [
@@ -63,27 +87,14 @@ if __name__ == "__main__":
         print("pytest failed.")
         sys.exit(1)
 
-    print("========== Generating docs...")
-    proc = run(["sphinx-build", ".", "docs", "-c", "docs"])
-    if proc.returncode != 0:
-        print("sphinx-build failed.")
-        sys.exit(1)
-    fix_crlf(Path("."))
 
-    print("========== Checking if anything changed...")
-    porcelain_after = (
-        run(["git", "status", "--porcelain"], capture_output=True)
-        .stdout.decode("ascii", errors="ignore")
-        .strip()
-    )
-    if porcelain_after:
-        print(porcelain_after)
-        run(["git", "status", "-vvv"])
-        print(
-            "Repo check caused some files to change.\n"
-            "If this is run locally, please commit the files that were just updated.\n"
-            "If this is in CI, please run python repo_check.py locally and commit the changes."
-        )
-        sys.exit(1)
-
+if __name__ == "__main__":
+    fix_crlf()
+    check_clean()
+    format()
+    pytest()
+    doctest()
+    build_docs()
+    fix_crlf()
+    check_clean()
     print("OK, everything is up to date")
